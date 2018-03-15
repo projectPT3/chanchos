710852b2533fab1bf090a9d827e97d11ad901732
Nadia Adibah <Nadia Adibah@LAPTOP-IITA7EUQ>
2018-03-15 22:03:01 +0800

    Add project files.

---
+++ chanchos/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(chanchos.Startup))]
+namespace chanchos
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
