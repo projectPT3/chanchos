1c2c2531b3a3f8ebb36141cfdeb5123ca142b3e0
nadiadibah <32533323+nadiadibah@users.noreply.github.com>
2018-03-21 20:38:18 +0800

    Add files via upload

---
+++ projectPT3/Models/Employer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelloWorldMVC.Models
+{
+    public class Employer
+    {
+        public string companyName { get; set; }
+        public string address { get; set; }
+        public int contactNum { get; set;  }
+
+        public void display ()
+        {
+
+        }
+    }
+}
---
+++ projectPT3/Models/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelloWorldMVC.Models
+{
+    public class Person
+    {
+        public string name { get; set; }
+        public int yob { get; set; }
+        public int age { get; set; }
+        public string category { get; set; }
+
+        public void calculate()
+        {
+            age = DateTime.Today.Year - yob;
+
+            if (age < 13)
+            {
+                category = "child";
+            }
+            else if (age > 12 && age < 20)
+            {
+                category = "teenager";
+            }
+           else if (age > 19 && age < 60)
+            {
+                category = "adult";
+            }
+            else if (age > 59)
+            {
+                category = "senior";
+            }
+            else { }
+        }
+    }
+}
