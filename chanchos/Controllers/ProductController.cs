668396aef68cdb7d3ea810297dcf9356b5d2db8b
Nadia Adibah <Nadia Adibah@LAPTOP-IITA7EUQ>
2018-03-21 23:14:14 +0800

    porduct

---
+++ chanchos/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using chanchos.Models;
+
+namespace chanchos.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult addProduct()
+        {
+            return View("addProductForm");
+        }
+
+
+        public ActionResult addProduct(Product p)
+        {
+            Product pp = new Product()
+            {
+                pid = "p001",
+                pname = "basikal",
+                price = 30.00
+            };
+
+            return View(pp);
+
+        }
+    }
+}
