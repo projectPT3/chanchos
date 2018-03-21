1c5938efe4ac2954c3e89e9c981161d4031d5c8e
shafri <mohamad.shafri.amir@gmail.com>
2018-03-21 21:11:12 +0800

    biling, payment chechout controller and view

---
+++ chanchos/Models/Payment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace chanchos.Models
+{
+	public class Payment
+	{
+		String bankName { get; set; }
+		String acctNo { get; set; }
+		String totalPrice { get; set; }
+	}
+}
