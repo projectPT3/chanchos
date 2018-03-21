7a9c6a55f334a7385939cb561eb05632e5581f30
Nadia Adibah <Nadia Adibah@LAPTOP-IITA7EUQ>
2018-03-21 23:24:50 +0800

    order

---
+++ chanchos/Controllers/OrderController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using chanchos.Models;
+
+namespace chanchos.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult makeOrder()
+        {
+            return View("OrderForm");
+        }
+
+        public ActionResult makeOrder(Order o)
+        {
+            Order O = new Order();
+            return View(O);
+        }
+    }
+}
---
+++ chanchos/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace chanchos.Models
+{
+    public class Order
+    {
+        public string orderID;
+        public DateTime orderDate;
+        public Product product;
+
+        public void display()
+        {
+
+        }
+    }
+}
