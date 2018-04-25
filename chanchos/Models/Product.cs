77e2e2abe1bdb7fce42a968403dd65387da14d48
NUR LIYANA BINTI MOHD.ARIS <nliyana223@live.utm.my>
2018-04-25 23:30:25 +0800

    try

--- chanchos/Models/Product.cs
+++ chanchos/Models/Product.cs
@@ -5,7 +5,7 @@
 //     Manual changes to this file may cause unexpected behavior in your application.
 //     Manual changes to this file will be overwritten if the code is regenerated.
 // </auto-generated>
-//------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------
 
 namespace chanchos.Models
 {
